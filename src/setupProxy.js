416b591d21ab04d22d62c68d1528c72bfdc7357f
MemoryNoodles <729804002@qq.com>
2019-03-19 10:56:30 +0800

    。。

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
     const options = {
          target: 'http://192.168.7.146:12345', // 目标服务器 host
-       // target: 'http://localhost:5555', // 目标服务器 host
+       // target: 'http://localhost:5555', // 目标服务器 host试试
         pathRewrite: {'^/api' : ''},
         changeOrigin: true,
     };
